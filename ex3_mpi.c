6f77dc2103d4b9f1ea459850e987d452b19ec97b
Bertone-Porto <38756105+Bertone-Porto@users.noreply.github.com>
2023-02-16 13:30:37 -0300

    Update ex3_mpi.c

--- ex3_mpi.c
+++ ex3_mpi.c
@@ -20,28 +20,10 @@ como um parâmetro na linha de comando.
 #include <stdio.h>
 #include <stdlib.h>
 
-int power(int x, int y){
-    if (y == 0) return 1;
-    int temp = x;
-    while (--y > 0){
-        x *= temp;
-    } return x;
-}
-
-double atoint(char * arg){
-   int i, value, sizeArgv;
-   value = 0; sizeArgv = 0;
-   for (i = 0; arg[i] != '\0'; i++)
-      sizeArgv += 1;
-   for (i = 0; i < sizeArgv; i++)
-      value +=  (arg[i] - '0') * power(10,sizeArgv-i-1);
-   return value;
-}
-
 int main(int argc,char *argv[]) {
    int numtasks, rank, dest, source, count, tag = 0;
-   int inmsg, outmsg, cont, i;
-   //int cont =0;
+   int inmsg, outmsg, i;
+   int cont = 0;
    double tamanho; double trocas;
    double * vet;
    MPI_Status Stat;
@@ -50,8 +32,7 @@ int main(int argc,char *argv[]) {
    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-   tamanho = atoint(argv[1]); trocas = atoint(argv[2]);
-	//tamanho = atoi(argv[1]); trocas = atoi(argv[2]);
+	tamanho = atoi(argv[1]); trocas = atoi(argv[2]);
 
    vet = (double*) malloc(tamanho * sizeof(double*));
    for (i = 0; i < tamanho; i++) 
@@ -61,19 +42,18 @@ int main(int argc,char *argv[]) {
       double before = MPI_Wtime();
       while (cont++ < trocas){
          for (i = 0; i < tamanho; i++) 
-            vet[i] = 4;
+            vet[i] = 2;
          MPI_Send(vet, tamanho, MPI_DOUBLE, 1, tag, MPI_COMM_WORLD);
       	 MPI_Recv(vet, tamanho, MPI_DOUBLE, 1, tag, MPI_COMM_WORLD, &Stat);
       } 
       double final_time = MPI_Wtime() - before; 
       printf("Tempo de execução: %lf\n",final_time);
-      //cont=0
-      
-   } else {
+
+   } else if(rank == 1){
      while (cont++ < trocas){
         MPI_Recv(vet, tamanho, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &Stat);
         for (i = 0; i < tamanho; i++) 
-            vet[i] = 2;
+            vet[i] = 4;
         MPI_Send(vet, tamanho, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
      }  
    }
